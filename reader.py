d2899f123e71392cbeea8565434e1eee95c8753a
Dave Pedu <dave@interana.com>
2016-06-15 16:56:14 -0700

    Mostly working

--- reader.py
+++ reader.py
@@ -4,127 +4,28 @@ import logging
 logging.basicConfig(filename='test.log',level=logging.DEBUG)
 import json
 import urwid
-#from urwid.command_map import *
 
-#class HidablePile(urwid.Pile):
-#    def __init__(self, widget_list, focus_item=None, hidden=False):
-#        super(HidablePile, self).__init__(widget_list, focus_item=None)
-#        self.hidden = hidden
-#        self.hidden_contents = []
-#        if hidden:
-#            self.hide()
-#
-#    def hide(self):
-#        if not self.hidden:
-#            self.hidden = True
-#            for i in range(0, len(self.contents)):
-#                self.hidden_contents.append(self.contents.pop())
-#            self._invalidate()
-#
-#    def show(self):
-#        if self.hidden:
-#            self.hidden = False
-#            for i in range(0, len(self.hidden_contents)):
-#                self.contents.append(self.hidden_contents.pop())
-#            self._invalidate()
-#
-#    #def keypress(self, size, key):
-#    #    x, y = self.get_cursor_coords(size)
-#    #    if self.hidden:
-#    #        if key == 'down':
-#    #            if y > 0:
-#    #                key = super(HidablePile, self).keypress(size, key)
-#    #            else:
-#    #                logging.info('HidablePile: ' + key)
-#    #
-#    #            return key
-#    #    else:
-#    #        return super(HidablePile, self).keypress(size, key)
 
-class JsonBox(urwid.Text):
+class JsonBox(urwid.Edit):
+    """
+    An abused urwid.Edit widget. Inputs are blocked so it behaves as a urwid.Text that we can focus upon.
+    """
     def __init__(self, markup):
-        #urwid.Text(markup, align='left', wrap='space', layout=None)
         super(JsonBox, self).__init__(markup)
 
     def keypress(self, size, key):
         #logging.info('JsonBox: ' + key)
-        #return key
-        logging.info('JsonBox: ' + key)
-        return super(JsonBox, self).keypress(size, key)
-
-def walk_json(data, parent=None, depth=0, print_key=None):
-    """
-    Return a nested list of Padding + Piles/Texts representing the json
-    """
-    logging.info(type(data))
-    this_row = urwid.Pile([])
-    parent.contents.append((this_row, ('pack', None)))
-
-    if type(data) in [dict, list]:
-        begin_char = '{' if type(data) is dict else '['
-        end_char = '}' if type(data) is dict else ']'
-
-        this_row.contents.append(
-            (
-                urwid.Padding(
-                    JsonBox('%s: %s'%(print_key, begin_char) if print_key else begin_char),
-                    left=5*depth
-                ),
-                ('pack', None)
-            )
-        )
-
-        if type(data) is dict:
-            for key in sorted(data.keys()):
-                walk_json(data[key], this_row, depth+1, key)
-        else:
-            for value in data:
-                walk_json(value, this_row, depth+1)
-
-        this_row.contents.append(
-            (
-                urwid.Padding(
-                    JsonBox(end_char),
-                    left=5*depth
-                ),
-                ('pack', None)
-            )
-        )
-
-    else:
-        if data is None:
-            data = 'null'
-        this_row.contents.append(
-            (
-                urwid.Padding(
-                    JsonBox('%s: %s'%(print_key, str(data)) if print_key else str(data)),
-                    left=5*depth
-                ),
-                ('pack', None)
-            )
-        )
-    #if depth > 0:
-    #    this_row.hide()
-    return this_row
-
-
-header_buttons = [
-    ('fixed', 13, urwid.Button("(F1) Help")),
-    ('fixed', 13, urwid.Button("(F2) Load")),
-    ('fixed', 15, urwid.Button("(F3) Export")),
-    ('fixed', 15, urwid.Button("(F4) Filter")),
-    ('fixed', 13, urwid.Button("(F5) Exit")),
-]
-
-_navColumns = urwid.Columns(header_buttons, dividechars=3, focus_column=None, min_width=1, box_columns=None)
-
-navColumns = urwid.AttrMap(_navColumns, 'header')
+        return key
 
 
 class JsonObject(urwid.Pile):
-    def __init__(self, json_object, preview_keys=['_t', 'severity', 'event_name']):
+    """
+    A pile that can be collapsed to a single row aka "hidden". these are nested for sub-objects
+    """
+    def __init__(self, json_object, preview_keys=['_t', 'severity', 'event_name'], print_key=None, hidden=False):
         super(JsonObject, self).__init__([])
         self.json = json_object
+        self.print_key = print_key
 
         preview_text = "{ ... }"
         if type(self.json) is dict:
@@ -133,19 +34,77 @@ class JsonObject(urwid.Pile):
                 if key in self.json:
                     preview_text+="{}: {}, ".format(key, self.json[key])
             preview_text += '... }'
-        
-        self.hidden_item = urwid.Pile([urwid.Text(preview_text)])
-        self.json_item = walk_json(json_object, parent=urwid.Pile([]))
+        elif type(self.json) is list:
+            preview_text = "[ ... ]"
 
-        self.is_hidden = True
+        if print_key:
+            preview_text = '{}: {}'.format(print_key, preview_text)
 
-        self.contents.append((self.hidden_item, ('weight', 1)))
+        self.hidden_item = urwid.Padding(urwid.Pile([JsonBox(preview_text)]), left=5)
+
+        #self.json_item = walk_json(json_object, parent=urwid.Pile([]))
+        widgets = []
+        if type(self.json) is dict:
+            widgets.append(urwid.AttrMap(
+                JsonBox( (print_key+": {") if print_key else '{'),
+                'json_row',
+                'json_row_h'
+            ))
+
+        elif type(self.json) is list:
+            widgets.append(urwid.AttrMap(
+                JsonBox( (print_key+": [") if print_key else '['),
+                'json_row',
+                'json_row_h'
+            ))
+
+
+        if type(self.json) in [list, dict]:
+            if type(self.json) is list:
+                for item in self.json:
+                    widgets.append(JsonObject(item))
+            elif type(self.json) is dict:
+                for key in sorted(self.json.keys()):
+                    widgets.append(JsonObject(self.json[key], hidden=type(self.json[key]) in [list, dict], print_key=key))
+
+        else: # plain value
+            if type(self.json) == type(None):
+                str_value = 'null'
+            elif type(self.json) not in [int, float]:
+                str_value = '"{}"'.format(self.json)
+            else:
+                str_value = self.json
+            widgets.append(JsonBox(
+                '{}: {},'.format(print_key, str_value) if print_key else '{},'.format(str(str_value))
+            ))
+
+
+
+
+
+        if type(self.json) is dict:
+            widgets.append(urwid.AttrMap(
+                JsonBox('}'),
+                'json_row',
+                'json_row_h'
+            ))
+        elif type(self.json) is list:
+            widgets.append(urwid.AttrMap(
+                JsonBox("]"),
+                'json_row',
+                'json_row_h'
+            ))
+        self.json_item = urwid.Padding(urwid.Pile(widgets), left=5)
+
+        self.is_hidden = hidden
+
+        self.contents.append(((self.hidden_item if hidden else self.json_item), ('weight', 1)))
 
     def selectable(self):
         return True
 
-    def rebuild(self):
-        pass
+    def can_hide(self):
+        return type(self.json) in [list,dict]
 
     def toggleHidden(self):
         self.contents.clear()
@@ -163,15 +122,24 @@ class JsonObject(urwid.Pile):
             self.toggleHidden()
 
     def keypress(self, size, key):
-        logging.info('JTR: ' + key)
+        if not self.is_hidden:
+            key = super(JsonObject, self).keypress(size, key)
+
+        logging.info('JsonObject('+ str(self.print_key) +'): ' + str(key))
         if key == ' ':
-            self.toggleHidden()
+            if self.can_hide():
+                self.toggleHidden()
+            return None
         elif key == 'right':
-            self.setHidden(False)
+            if self.can_hide():
+                self.setHidden(False)
+            return None
         elif key == 'left':
-            self.setHidden(True)
-        else:
-            return super(JsonObject, self).keypress(size, key)
+            if self.can_hide():
+                self.setHidden(True)
+            return None
+
+        return key
 
 
 
@@ -185,7 +153,7 @@ class JsonFileDisplay(urwid.ListBox):
         for i in range(0, self.num_lines):
             body.append(
                 urwid.AttrMap(
-                    JsonObject(self.next_line()),
+                    JsonObject(self.next_line(), hidden=True),
                 #    'row_odd' if self.current_line % 2 == 0 else 'row_even'
                      'json_row',
                      'json_row_h'
@@ -200,8 +168,9 @@ class JsonFileDisplay(urwid.ListBox):
         return json.loads(self.file.readline())
 
     def keypress(self, size, key):
-        logging.info('JsonListBox: ' + key)
-        return super(JsonFileDisplay, self).keypress(size, key)
+        result = super(JsonFileDisplay, self).keypress(size, key)
+        logging.info('JsonFileDisplay: ' + key + '->' + str(result))
+        return result
 #        key = super(JsonListBox, self).keypress(size, key)
 #        if key != 'enter':
 #            return key
@@ -219,6 +188,17 @@ class JsonFileDisplay(urwid.ListBox):
 #            self.body.pop(0)
 #            logging.info("popped")
 
+header_buttons = [
+    ('fixed', 13, urwid.Button("(F1) Help")),
+    ('fixed', 13, urwid.Button("(F2) Load")),
+    ('fixed', 15, urwid.Button("(F3) Export")),
+    ('fixed', 15, urwid.Button("(F4) Filter")),
+    ('fixed', 13, urwid.Button("(F5) Exit")),
+]
+
+_navColumns = urwid.Columns(header_buttons, dividechars=3, focus_column=None, min_width=1, box_columns=None)
+
+navColumns = urwid.AttrMap(_navColumns, 'header')
 
 mainFrame = urwid.Frame(JsonFileDisplay('./query_api_server.log.20160527T012105'), header=navColumns, footer=None, focus_part='body')
 
@@ -227,10 +207,11 @@ palette = [
     ('I say', 'default,bold', 'default'),
     ('header', 'black', 'light green'),
     ('row_odd', 'white', 'dark gray'),
-    
-    ('json_row', 'white', ''),
-    ('json_row_h', 'black', 'dark cyan')
-    #('json_row_h', 'light gray', 'dark cyan', 'standout','#ff8', '#806')
+
+    ('json_row', 'dark gray', 'black'),
+    ('json_row_h', 'white', 'black')
+    #('json_row_h', 'light gray', 'dark blue', 'standout','#ff8', '#806')
+    #('json_row_h', 'white', 'dark gray', '','#f0f', '#00f')
 ]
 
 screen = urwid.raw_display.Screen()
